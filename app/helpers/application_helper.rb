8f8b7e2e36a85ac77bdd80f9c9478c578279d38a
quan <thanhquan.ng@gmail.com>
2017-03-20 01:07:58 +0700

    Add logo, slide image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def logo_image_url
     image_path('FamTimeLogo.png')
   end
+
+  def slide_image_url1
+    image_path('happy1.jpg')
+  end
+
+  def slide_image_url2
+    image_path('happy2.jpg')
+  end
+
+  def slide_image_url3
+    image_path('happy3.jpg')
+  end
 end
