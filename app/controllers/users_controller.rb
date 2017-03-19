b3a841f3eea6a1d7fa56fa98729b8e566d8a17e6
quan <thanhquan.ng@gmail.com>
2017-03-19 17:25:32 +0700

    Sign Up done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcom$ to FamTime community, " + @user.first_name
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
   def user_params
-    params.require(:user_info).permit(:first_name, :email, :last_name, :password, :address, :tel)
+    params.require(:user).permit(:first_name, :email, :last_name, :password, :address, :tel)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, :first_name, :last_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, presence: true
 end
