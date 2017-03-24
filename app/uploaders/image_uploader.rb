7a1c3dd952f6df15522db1a7d2cf95a701a2503c
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-03-24 16:37:09 +0700

    added aws and dotenv gem

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  #include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+
+  storage :aws # or you can comment out this line because config.storage is already :aws
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -36,14 +36,13 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-   def extension_whitelist
+  def extension_whitelist
     %w(jpg jpeg gif png)
-   end
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
