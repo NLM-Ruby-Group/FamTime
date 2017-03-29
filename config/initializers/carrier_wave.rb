21c070e3a0c2df3b8bd7feb137c49ad301da9860
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-03-29 10:19:20 +0700

    removed remove_image

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,10 @@
 # require 'carrierwave/orm/activerecord'
+if Rails.env.production? 
 
    CarrierWave.configure do |config|
-
-     if Rails.env.development? || Rails.env.test?
-       config.storage = :file
-    else
      config.storage    = :aws
      config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
      config.aws_acl    = 'public-read'
-
      # The maximum period for authenticated_urls is only 7 days.
      config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
@@ -23,5 +19,9 @@
        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
        region:            ENV.fetch('AWS_REGION') # Required
      }
-  end
+     end
+    else
+      CarrierWave.configure do |config|
+      config.storage = :file
+    end
    end
